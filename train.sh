f855890220d4c5e65d09014855cd2726a19d76d4
ida-zrt <ida_zrt@163.com>
2020-11-21 13:58:26 +0800

    fix path

--- train.sh
+++ train.sh
@@ -10,5 +10,5 @@ module load anaconda3/2019.07
 module unload cuda/10.0
 module load cuda/9.0
 source activate xiaor
-python -u train.py --tub ../data/train_test/tub --model ./models/mypilot
+python -u train.py --tub ../data/data_11_20_1521/tub/ --model ./models/mypilot2
 # python -u modelconvert.py
